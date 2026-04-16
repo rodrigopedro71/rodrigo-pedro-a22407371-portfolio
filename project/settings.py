d6215109d93b25f7e06566fdc5af8864ec481694
rodrigopedro71 <a22407371@alunos.ulht.pt>
2026-04-16 09:06:55 +0000

    Pagina Escola com Cursos, Professores e Alunos

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "portfolio",
+    "escola",
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+import os
+
+# Pasta no servidor onde os ficheiros vão ser guardados
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
